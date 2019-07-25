d8809381a1b4ab01123494ef1f96be9edb89d8f8
jyc <1357602588@qq.com>
2019-07-25 22:19:44 +0800

    ---first---

---
+++ src/main/java/com/demo/dubbo_springboot_provider/DubboSpringbootProviderApplication.java
@@ -0,0 +1,13 @@
+package com.demo.dubbo_springboot_provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboSpringbootProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboSpringbootProviderApplication.class, args);
+    }
+
+}
